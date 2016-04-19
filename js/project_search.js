b59f495c952f140f35bdd89db41925e02974469f
Thomas Pakh <thomas.pakh@gmail.com>
2016-04-18 20:23:24 -0400

    Lots of tweaks. Added some services

--- js/project_search.js
+++ js/project_search.js
@@ -1,45 +1,44 @@
-// TODO: Add semicolons
-$(function() {
-    var projects = []
+$(function () {
+    var projects = [];
 
-    $('#projects > li > a:first-child').each(function(index) {
-        projects.push($(this).text())
+    $('#projects > li > a:first-child').each(function (index) {
+        projects.push($(this).text());
     })
 
-    var f = new Fuse(projects)
+    var f = new Fuse(projects);
 
     function handleSearch() {
         function toggleAll(bool) {
-            $('#projects > li').each(function(index) {
-                $(this).toggle(bool)
-            })
+            $('#projects > li').each(function (index) {
+                $(this).toggle(bool);
+            });
         }
 
-        var search = $.trim($("#search-projects").val())
+        var search = $.trim($("#search-projects").val());
         if (search == null || search == '') {
-            toggleAll(true)
-            return
+            toggleAll(true);
+            return;
         }
-        search = search.substring(0, 32) // Fuse suports a maximum length of 32
-        var result = f.search(search)
+        search = search.substring(0, 32); // Fuse suports a maximum length of 32
+        var result = f.search(search);
         if (result.length === 0) {
-            toggleAll(false)
+            toggleAll(false);
         } else {
-            $('#projects > li').each(function(index) {
-                $("#search-projects").toggle(!$.inArray(index, result))
-            })
+            $('#projects > li').each(function (index) {
+                $("#search-projects").toggle(!$.inArray(index, result));
+            });
         }
     }
 
     $('#search-projects').keyup(handleSearch);
 
-    $('.search-form .form-group').hover(function() { 
-        $( this ).toggleClass("hover", true);    
+    $('.search-form .form-group').hover(function () {
+        $(this).toggleClass("hover", true);
     },
-    function() {
-        var search = $.trim($("#search-projects").val());
-        if (search == null || search == '') {
-            $( this ).toggleClass("hover", false);
-        }
-    });
-})
+        function () {
+            var search = $.trim($("#search-projects").val());
+            if (search == null || search == '') {
+                $(this).toggleClass("hover", false);
+            }
+        });
+});
