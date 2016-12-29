ac0beee2d101d9cb31a7301ae43452510770f08d
Thomas Pakh <thomas.pakh@gmail.com>
2016-12-29 13:07:25 -0500

    Add description capability to gallery viewer

--- js/imgur_access.js
+++ js/imgur_access.js
@@ -10,8 +10,8 @@ String.prototype.format = function () {
 };
 
 $(function () {
-    function getAlbumImages(id, success, error) {
-        $.ajax({
+    function getAlbumImages(id) {
+        return $.ajax({
             url: 'https://api.imgur.com/3/album/' + encodeURIComponent(id) + '/images',
             localCache: true, // localStorage.clear() to reset this cache
             cacheTTL: 12, // In hours.
@@ -23,9 +23,7 @@ $(function () {
                 Authorization: 'Client-ID ' + clientId,
                 Accept: 'application/json'
             }
-        }).done(function (response) {
-            success(response.data)
-        }).fail(error);
+        });
     }
 
     var $images = $('#images');
@@ -34,21 +32,39 @@ $(function () {
         var id = $images.data('album-id');
         const thumbnailChars = ['s', 'b', 't', 'm', 'l', 'h'];
         var thumbnailChar = thumbnailChars[$images.data('thumbnail-size')] || 'b';
-        getAlbumImages(id, function (images) {
+        getAlbumImages(id).done(function (response) {
+            var images = response.data;
             images.forEach(function (image, index) {
                 var index = image.link.lastIndexOf('.');
                 var thumbnail = image.link.slice(0, index) + thumbnailChar + image.link.slice(index);
                 var title = !image.title || image.title == 'null' ? undefined : image.title;
                 var description = !image.description || image.description == 'null' ? undefined : image.description;
                 $('<a/>')
-                    .append($('<img>').prop('src', thumbnail).prop('alt', description))
+                    .append($('<img>').prop('src', thumbnail).prop('alt', title))
                     .prop('href', image.link)
-                    .prop('title', description)
+                    .prop('title', title)
+                    .data('description', description)
                     .attr('data-gallery', '')
                     .appendTo($images)
             });
         });
     }
+
+    var $gallery = $('#blueimp-gallery');
+    $gallery
+        .on('open', function (event) {
+            $.extend($gallery.data('gallery').options, {
+                preloadRange: 4,
+                unloadElements: false,
+                clearSlides: false,
+                stretchImages: true,
+                fullScreen: false
+            });
+        })
+        .on('slide', function (event, index, slide) {
+            var text = $($gallery.data('gallery').list[index]).data('description');
+            $gallery.find('.description').text(text || '');
+        });
 
     loadImages();
 });
