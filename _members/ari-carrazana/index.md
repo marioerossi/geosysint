8ecd85a2358b82349171a6a47f4e90e99e178c89
Thomas Pakh <thomas.pakh@gmail.com>
2017-03-16 01:32:20 -0400

    Add search to papers page along with light cleanup
    * Fix navposition of pages
    * Remove hard-coded paper links in the papers page
    * Remove projects from individual sub-directories
    * Make projects_search.js slightly more modular (so that it can be used for papers too)

--- _members/ari-carrazana/index.md
+++ _members/ari-carrazana/index.md
@@ -3,4 +3,9 @@ layout: page
 title: Ariana Carrazana
 ---
 
+<ol class="breadcrumb">
+    <li><a href="{{site.baseurl}}/personnel/">Presonnel</a></li>
+    <li class="active">{{ page.title }}</li>
+</ol>
+
 ![Ariana Carrazana Info](ari-carrazana-info.png)
---
+++ js/search.js
@@ -0,0 +1,74 @@
+$(function () {
+    if (!search) {
+        console.warn("Search parameters not defined!")
+        return;
+    }
+    var searchTextBox = search.searchTextBox;
+    var searchList = search.searchList;
+
+    var inputBox = $(searchTextBox);
+
+    var projectsList = $(searchList);
+    var projects = $(searchList + " > li");
+    var projectsTitles = [];
+
+    projects.each(function (index) {
+        projectsTitles.push({
+            index: index, // Used by Fuse as an id
+            text: this.innerText // Used by Fuse for search
+        });
+    });
+
+    var options = {
+        id: "index",
+        caseSensitive: false,
+        shouldSort: true,
+        matchAllTokens: true,
+        threshold: 0.4,
+        location: 0,
+        distance: 50,
+        maxPatternLength: 32,
+        keys: ["text"],
+        verbose: false
+    };
+    var fuse = new Fuse(projectsTitles, options);
+
+    function handleSearch() {
+        var search = $.trim(inputBox.val()).substring(0, options.maxPatternLength);
+        if (search === "" || search === null) {
+            projects.detach("li"); // Remove all items
+            for (var i = 0; i < projects.length; i++) // Add back all items
+                projectsList.append(projects[i]);
+        } else {
+            var result = fuse.search(search);
+            projects.detach("li"); // Remove all items
+            for (var i = 0; i < result.length; i++) // Add back items returned from search
+                projectsList.append(projects[result[i]]);
+        }
+    }
+
+
+    var oldInput = inputBox.val();
+    inputBox.on("input", function () {
+        var newVal = inputBox.val();
+        if (oldInput != newVal) {
+            handleSearch();
+            oldInput = newVal
+        }
+    });
+
+    function hoverOn() {
+        $(this).toggleClass("hover", true);
+    }
+
+    function hoverOff() {
+        var search = $.trim(inputBox.val());
+        if (search === null || search === '') {
+            $(this).toggleClass("hover", false);
+        }
+    }
+
+    $('.search-form .form-group').hover(hoverOn, hoverOff);
+    $('.search-form .form-group').focusin(hoverOn);
+    $('.search-form .form-group').focusout(hoverOff);
+});
